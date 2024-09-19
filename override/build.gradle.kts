387090d04e33ad48fec7ab419e715acec5681848
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-19 10:00:08 +0100

    move code overrides to new project

--- override/build.gradle.kts
+++ override/build.gradle.kts
@@ -5,3 +5,7 @@ plugins {
 dependencies {
     implementation("com.github.beanio:beanio:3.1.0")
 }
+
+springBoot {
+    mainClass.set("uk.co.bluegecko.override.Main")
+}
---
+++ override/src/main/java/uk/co/bluegecko/override/Main.java
@@ -0,0 +1,5 @@
+package uk.co.bluegecko.override;
+
+public class Main {
+
+}
