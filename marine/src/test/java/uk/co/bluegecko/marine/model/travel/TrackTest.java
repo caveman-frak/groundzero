d725c3cfb183cf143b124e9474f5a4ea8a233adf
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-19 08:50:52 +0100

    use coordinate in trace

--- marine/src/test/java/uk/co/bluegecko/marine/model/travel/TrackTest.java
+++ marine/src/test/java/uk/co/bluegecko/marine/model/travel/TrackTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.offset;
 import static org.assertj.core.api.InstanceOfAssertFactories.DOUBLE;
 
+import com.uber.h3core.AreaUnit;
 import com.uber.h3core.H3Core;
 import java.awt.geom.Point2D;
 import java.io.IOException;
@@ -12,7 +13,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -51,7 +51,9 @@ class TrackTest extends AbstractTest {
 				.element(10, DOUBLE).isCloseTo(0.018, offset(0.000001));
 		assertThat(traces).hasSize(11).extracting(Trace::longitude)
 				.element(10, DOUBLE).isCloseTo(0.018, offset(0.000001));
-		assertThat(clock.instant()).isEqualTo(Instant.ofEpochMilli(1577886000000L));
+		assertThat(clock.instant()).isEqualTo(Instant.ofEpochMilli(1577886000000L))
+				.isBetween(LocalDateTime.of(2020, Month.JANUARY, 1, 13, 0).toInstant(ZoneOffset.UTC),
+						LocalDateTime.of(2020, Month.JANUARY, 1, 14, 0).toInstant(ZoneOffset.UTC));
 	}
 
 	@Test
@@ -59,16 +61,27 @@ class TrackTest extends AbstractTest {
 		Resolution resolution = Resolution.MEDIUM;
 		Partition partition = resolution.partition(h3Core, traces.get(1));
 		long epochIntervals = partition.epochIntervals();
-		assertThat(Instant.EPOCH.plus(epochIntervals, ChronoUnit.HOURS)).isEqualTo(
+		assertThat(Instant.EPOCH.plus(resolution.duration().multipliedBy(epochIntervals))).isEqualTo(
 				LocalDateTime.of(2020, Month.JANUARY, 1, 12, 0).toInstant(ZoneOffset.UTC));
 		assertThat(resolution.start(epochIntervals)).isEqualTo(
 				LocalDateTime.of(2020, Month.JANUARY, 1, 12, 0).toInstant(ZoneOffset.UTC));
 		assertThat(resolution.end(epochIntervals)).isEqualTo(
 				LocalDateTime.of(2020, Month.JANUARY, 1, 13, 0).toInstant(ZoneOffset.UTC));
+		assertThat(resolution.duration()).isEqualTo(Duration.ofHours(1));
 		long h3Cell = partition.h3Cell();
 		assertThat(h3Cell).isEqualTo(596538564771053567L);
 		assertThat(h3Core.getResolution(h3Cell)).isEqualTo(4);
 		assertThat(h3Core.h3ToString(h3Cell)).isEqualTo("84754a9ffffffff");
+		assertThat(h3Core.cellArea(h3Cell, AreaUnit.rads2)).isCloseTo(0.0000324883, offset(0.000001));
+		assertThat(h3Core.cellArea(h3Cell, AreaUnit.km2)).isCloseTo(1318.6944907, offset(0.000001));
+		assertThat(h3Core.cellToBoundary(h3Cell)).hasSize(6);
+		assertThat(h3Core.gridDisk(h3Cell, 1)).hasSize(7).contains(h3Cell)
+				.contains(596538831059025919L, 596538581950922751L, 596538530411315199L,
+						596538573360988159L, 596538410152230911L, 596538813879156735L)
+				.extracting(l -> h3Core.h3ToString(l)).contains("84754a9ffffffff")
+				.contains("84754e7ffffffff", "84754adffffffff", "84754a1ffffffff", "84754abffffffff", "8475485ffffffff",
+						"84754e3ffffffff");
+
 	}
 
 	@Test
@@ -76,12 +89,12 @@ class TrackTest extends AbstractTest {
 		List<Track> tracks = Track.tracks(h3Core, Resolution.FINE, traces);
 		assertThat(tracks).hasSize(11)
 				.extracting(Track::getH3Cell)
-				.containsExactlyInAnyOrder(605546023066009599L, 605546023066009599L, 605546023066009599L,
-						605546023066009599L, 605546023066009599L, 605545760670351359L, 605546023602880511L,
-						605545760670351359L, 605546023602880511L, 605545760670351359L, 605546023066009599L);
+				.containsOnly(605546023066009599L, 605545760670351359L, 605546023602880511L)
+				.extracting(l -> h3Core.h3ToString(l))
+				.contains("86754e64fffffff", "86754e66fffffff", "86754a937ffffff");
 		assertThat(tracks).extracting(Track::getEpochIntervals)
-				.containsExactlyInAnyOrder(2629809L, 2629806L, 2629810L, 2629807L, 2629808L, 2629805L, 2629801L,
-						2629802L, 2629803L, 2629804L, 2629800L);
+				.containsExactlyInAnyOrder(2629800L, 2629801L, 2629802L, 2629803L, 2629804L, 2629805L, 2629806L,
+						2629807L, 2629808L, 2629809L, 2629810L);
 		assertThat(tracks).extracting(Track::getTraces).extracting(Collection::size)
 				.containsExactly(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 		assertThat(tracks).extracting(Track::getEarliest).extracting(Instant::toString)
@@ -95,9 +108,9 @@ class TrackTest extends AbstractTest {
 		List<Track> tracks = Track.tracks(h3Core, Resolution.MEDIUM, traces);
 		assertThat(tracks).hasSize(3)
 				.extracting(Track::getH3Cell)
-				.containsExactlyInAnyOrder(596538564771053567L, 596538831059025919L, 596538564771053567L);
+				.containsOnly(596538564771053567L, 596538831059025919L);
 		assertThat(tracks).extracting(Track::getEpochIntervals)
-				.containsExactlyInAnyOrder(438300L, 438301L, 438301L);
+				.containsOnly(438300L, 438301L);
 		assertThat(tracks).extracting(Track::getTraces).extracting(Collection::size)
 				.containsExactlyInAnyOrder(2, 6, 3);
 		assertThat(tracks).extracting(Track::getEarliest).extracting(Instant::toString)
